7896b8f210d01f50007b3a83c41a0d7accb12a0d
chaeeunj <official2930@naver.com>
2023-06-05 16:39:32 +0900

    요구사항1, 2 구현

--- template/app.js
+++ template/app.js
@@ -8,7 +8,7 @@ $containers.forEach(($container, i) => {
   StarRating($container);
 
   // 이벤트 'rating-change'를 캐치해 화면에 표시한다.
-  $container.addEventListener('rating-change', e => {
+  $container.addEventListener('rating-change', (e) => {
     const rating = e.detail;
     $currentRatings[i].textContent = rating;
   });
--- template/star-rating/index.js
+++ template/star-rating/index.js
@@ -1,1 +1,42 @@
 // do something!
+
+const StarRating = () => {
+  const addStarRatingCSS = () => {
+    const starRatingCSSPAth = 'star-rating/theme.css';
+    const link = document.createElement('link');
+    link.href = starRatingCSSPAth;
+    link.rel = 'StyleSheet';
+
+    const lastLink = document.querySelector(
+      'link[rel="stylesheet"]:last-of-type'
+    );
+    lastLink.parentNode.insertBefore(link, lastLink.nextSibling);
+  };
+
+  const addStarRating = () => {
+    const stars = `
+      <div class='star-rating-container'>
+        <i class='bx bx-star'></i>
+        <i class='bx bx-star'></i>
+        <i class='bx bx-star'></i>
+        <i class='bx bx-star'></i>
+        <i class='bx bx-star'></i>
+      </div>
+    `;
+
+    document.querySelectorAll('.star-rating').forEach((container) => {
+      container.innerHTML = stars;
+    });
+  };
+
+  const init = () => {
+    window.addEventListener('DOMContentLoaded', () => {
+      addStarRatingCSS();
+      addStarRating();
+    });
+  };
+
+  init();
+};
+
+export default StarRating;
